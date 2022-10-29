b5e304383a5cbf442201cdf1bebf1fd5a518a1ac
Vinod Kotagiri <vinodkotagiri@icloud.com>
2022-10-29 21:34:17 +0530

    fixed reload issue in dashboard

--- src/pages/page.dashboard.jsx
+++ src/pages/page.dashboard.jsx
@@ -1,13 +1,34 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useEffect } from 'react'
 import AdminDashboard from '../components/component.admindashboard'
 import UserDashboard from '../components/component.userdashboard'
 import { useSelector } from 'react-redux'
+import Loader from '../components/component.loader'
+import { useState } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
 const DashboardPage = () => {
-	const user = useSelector((state) => state.auth.user)
+	const location = useLocation()
+	const navigate = useNavigate()
 
+	const user = useSelector((state) => state.auth.user)
+	const [loading, setLoading] = useState(false)
+
+	useEffect(() => {
+		setLoading(true)
+		navigate(location.pathname)
+		setTimeout(() => {
+			setLoading(false)
+		}, 2000)
+		if (!user) setLoading(true)
+	}, [])
 	return (
 		<Fragment>
-			{user.role === 1 ? <AdminDashboard /> : <UserDashboard />}
+			{loading ? (
+				<Loader />
+			) : (
+				<Fragment>
+					{user?.role === 1 ? <AdminDashboard /> : <UserDashboard />}
+				</Fragment>
+			)}
 		</Fragment>
 	)
 }
