bc9a35d5028508fa601bed01337b731ead59cf8d
Vinod Kotagiri <vinodkotagiri@icloud.com>
2022-11-02 19:45:55 +0530

    Final code

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-export const BASE_URL = 'http://localhost:12000/api' //'https://bizbook-server.onrender.com/api'
+export const BASE_URL = 'https://bizbook-server.onrender.com/api'
 
 export default axios.create({
 	baseURL: BASE_URL,
