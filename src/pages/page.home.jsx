c753ab1f8f8160591b71479668098ce83c44b280
Vinod Kotagiri <vinodkotagiri@icloud.com>
2022-11-29 23:15:34 +0530

    added loader to home page

--- src/pages/page.home.jsx
+++ src/pages/page.home.jsx
@@ -4,10 +4,11 @@ import ProductCard from '../components/component.productCard'
 import TitleHeader from '../components/component.titleHeader'
 import axios from '../services/axios'
 import Search from '../components/component.search'
+import Loader from '../components/component.loader'
 const HomePage = () => {
 	const [newArrivals, setNewArrivals] = useState([])
 	const [mostSelling, setMostSelling] = useState([])
-
+	const [loading, setLoading] = useState(true)
 	const getProductsByArrival = () => {
 		axios
 			.get('/products?sortBy=createdAt&order=desc&limit=6')
@@ -24,26 +25,32 @@ const HomePage = () => {
 		getProductsByArrival()
 		getProductsBySold()
 	}, [])
-
+	useEffect(() => {
+		if (newArrivals && mostSelling) setLoading(false)
+		else setLoading(true)
+	}, [newArrivals, mostSelling])
 	return (
 		<Fragment>
 			<TitleHeader title='BizBook.io' description='Home page ' />
 			<Search />
-
-			<Container fluid>
-				<h1 className='my-3 text-muted'>New Arrivals</h1>
-				<Row className='px-3'>
-					{newArrivals.map((product, index) => (
-						<ProductCard product={product} key={index} />
-					))}
-				</Row>
-				<h1 className='my-3 text-muted'>Most Selling</h1>
-				<Row className='px-3'>
-					{mostSelling.map((product, index) => (
-						<ProductCard product={product} key={index} />
-					))}
-				</Row>
-			</Container>
+			{loading ? (
+				<Loader />
+			) : (
+				<Container fluid>
+					<h1 className='my-3 text-muted'>New Arrivals</h1>
+					<Row className='px-3'>
+						{newArrivals.map((product, index) => (
+							<ProductCard product={product} key={index} />
+						))}
+					</Row>
+					<h1 className='my-3 text-muted'>Most Selling</h1>
+					<Row className='px-3'>
+						{mostSelling.map((product, index) => (
+							<ProductCard product={product} key={index} />
+						))}
+					</Row>
+				</Container>
+			)}
 		</Fragment>
 	)
 }
